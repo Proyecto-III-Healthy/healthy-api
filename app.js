5b8774cd31f6a2ea2d176cb28257d89980597026
Maribel <maribel.lorenzo.delafuente@gmail.com>
2024-07-10 19:07:49 +0200

    done

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     // methods: ["GET", "POST"],
     // allowedHeaders: ["Content-Type"],
     // credentials: true,
